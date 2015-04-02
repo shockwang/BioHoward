49255212a1a054e6df75d66f1261e4e8d30da72c
shockwang <henryandjay@hotmail.com>
2015-04-02 16:06:25 +0800

    2015/4/2 work
    add some event state

--- src/module/mission/chapter0/MainMission.java
+++ src/module/mission/chapter0/MainMission.java
@@ -9,7 +9,13 @@ public class MainMission implements IMission{
 		BEFORE_OPENING("開頭劇情前"),
 		AFTER_OPENING("開頭劇情後"),
 		AFTER_FIRST_BATTLE("正在教學任務中"),
-		START_SEARCHING("設法逃出宿舍，先去一樓大門看看吧。"),
+		START_SEARCHING("設法逃出宿舍，先去一樓大門看看。"),
+		FOUND_DOORS_BLOCKED("宿舍一樓的兩扇大門都被封住了，去宿舍管理中心找找看有沒什麼\n什麼有用的資源。"),
+		BEFORE_BREAK_MANAGE_DOOR("尋找適合用來打破宿舍管理中心門上玻璃的工具。"),
+		AFTER_BREAK_MANAGE_DOOR("爬進宿舍管理中心尋找可用的資源。"),
+		AFTER_FLEE_FROM_MANAGER("齋媽的攻擊十分凌厲，要如何應對呢? 或許可以試著拿滅火器噴她?"),
+		AFTER_DEFEAT_MANAGER("拿到宿舍鑰匙，去各個房間尋找可用的工具。"),
+		AFTER_FOUND_CUTTER("找到油壓剪了，去一樓房間找一個可破壞的鐵窗，剪斷後逃出去。"),
 		DONE("結束!");
 		
 		public String name;

38dbf5f700b7f59f7a3b59fb63b0e4b801e7b3d9
shockwang <henryandjay@hotmail.com>
2015-04-04 22:53:22 +0800

    2015/4/4 work
    add some room command

--- src/module/event/map/instance/chapter0/YiDormitoryRoomCommand.java
+++ src/module/event/map/instance/chapter0/YiDormitoryRoomCommand.java
@@ -1,37 +1,102 @@
 package module.event.map.instance.chapter0;
 
 import module.character.Group;
+import module.command.CommandServer;
 import module.event.api.IRoomCommand;
+import module.map.api.IRoom;
+import module.map.constants.CExit.exit;
 import module.mission.chapter0.MainMission;
 import module.server.PlayerServer;
 import module.utility.EventUtil;
 
 public class YiDormitoryRoomCommand {
-	public static void initialize(){
-		EventUtil.mapCommandMap.put("101,92,1", new IRoomCommand(){
-			
+	public static void initialize() {
+		EventUtil.mapCommandMap.put("101,92,1", new IRoomCommand() {
+
 			@Override
 			public boolean roomSpecialCommand(Group g, String[] msg) {
-				if (msg.length == 0 || !msg[0].equals("crash")) return false;
-				
-				MainMission mm = (MainMission) PlayerServer.getMissionMap().get(MainMission.class.toString());
-				if (mm.getState() == MainMission.State.BEFORE_BREAK_MANAGE_DOOR){
-					if (g.getInventory().findItem("fire") != null){
-						g.getAtRoom().informRoom(g.list.get(0).charList.get(0).getChiName()
-								+ "舉起滅火器，用力地往玻璃門砸下去! 匡噹!\n只見門上的玻璃碎了一地。\n");
-						mm.setState(MainMission.State.AFTER_BREAK_MANAGE_DOOR);
-					} else {
-						g.getAtRoom().informRoom(g.list.get(0).charList.get(0).getChiName()
-								+ "身上並沒有帶著適當的工具。\n");
+				if (msg.length == 0)
+					return false;
+
+				MainMission mm = (MainMission) PlayerServer.getMissionMap()
+						.get(MainMission.class.toString());
+				if (msg[0].equals("crash")) {
+					if (mm.getState() == MainMission.State.BEFORE_BREAK_MANAGE_DOOR) {
+						if (g.getInventory().findItem("fire") != null) {
+							g.getAtRoom()
+									.informRoom(
+											g.list.get(0).charList.get(0)
+													.getChiName()
+													+ "舉起滅火器，用力地往玻璃門砸下去! 匡噹!\n只見門上的玻璃碎了一地。\n");
+							mm.setState(MainMission.State.AFTER_BREAK_MANAGE_DOOR);
+						} else {
+							g.getAtRoom().informRoom(
+									g.list.get(0).charList.get(0).getChiName()
+											+ "身上並沒有帶著適當的工具。\n");
+						}
+					} else if (((MainMission.State) mm.getState()).index >= MainMission.State.AFTER_BREAK_MANAGE_DOOR.index) {
+						g.getAtRoom().informRoom("門上的玻璃已經被打碎了。\n");
 					}
 					return true;
-				} else if (((MainMission.State) mm.getState()).index 
-						>= MainMission.State.AFTER_BREAK_MANAGE_DOOR.index){
-					g.getAtRoom().informRoom("門上的玻璃已經被打碎了。\n");
+				} else if (msg[0].equals("climb")) {
+					synchronized (g.getAtRoom()) {
+						MainMission.State state = (MainMission.State) mm
+								.getState();
+						if (state.index < MainMission.State.AFTER_BREAK_MANAGE_DOOR.index) {
+							g.getAtRoom().informRoom("門上的玻璃還沒破呢，你想從哪爬進去?\n");
+						} else {
+							if (!g.getInBattle()) {
+								g.getAtRoom().informRoom(
+										String.format(
+												"%s跨過門上破碎玻璃窗的空隙，鑽進了宿舍管理中心。\n",
+												g.list.get(0).charList.get(0)
+														.getChiName()));
+								IRoom south = g.getAtRoom().getExits()
+										.get(exit.SOUTH).getRoom();
+								south.getGroupList().gList.add(g);
+								g.getAtRoom().getGroupList().gList.remove(g);
+								g.setAtRoom(south);
+								CommandServer.informGroup(g, 
+										south.displayRoomExceptGroup(g));
+							} else
+								CommandServer.informGroup(g, "隊伍正在戰鬥中喔!\n");
+						}
+						return true;
+					}
+				}
+				return false;
+			}
+		});
+		
+		EventUtil.mapCommandMap.put("101,91,1", new IRoomCommand() {
+
+			@Override
+			public boolean roomSpecialCommand(Group g, String[] msg) {
+				if (msg.length == 0) return false;
+				
+				if (msg[0].equals("climb")){
+					synchronized (g.getAtRoom()){
+						if (!g.getInBattle()) {
+							g.getAtRoom().informRoom(
+									String.format(
+											"%s跨過門上破碎玻璃窗的空隙，鑽出了宿舍管理中心。\n",
+											g.list.get(0).charList.get(0)
+													.getChiName()));
+							IRoom north = g.getAtRoom().getExits()
+									.get(exit.NORTH).getRoom();
+							north.getGroupList().gList.add(g);
+							g.getAtRoom().getGroupList().gList.remove(g);
+							g.setAtRoom(north);
+							CommandServer.informGroup(g, 
+									north.displayRoomExceptGroup(g));
+						} else
+							CommandServer.informGroup(g, "隊伍正在戰鬥中喔!\n");
+					}
 					return true;
 				}
 				return false;
 			}
+			
 		});
 	}
 }
